e7df67ff63bdc6a32161a90d8bb929b8568dbd2e
Rikki Guy <git@rikkiguy.me.uk>
2014-12-30 04:29:02 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exiled "0.1.0-SNAPSHOT"
+(defproject exiled "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
